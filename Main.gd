89b8c5d112c0f086886991ed6dc97574459b53a0
Rattja <krizz2003@hotmail.com>
2022-02-21 10:22:33 +0100

    Performance fix

--- Main.gd
+++ Main.gd
@@ -2,7 +2,8 @@ extends Node2D
 # Test text for github testtest 
 
 func _ready():
-	OS.vsync_enabled(true)
+#	OS.vsync_enabled(true)
+	OS.low_processor_usage_mode = true
 	Engine.target_fps = 30
 	get_tree().call_group("T1", "check_recipe")
 
